0dbb227eecac823c76b583f4e0d92ce66b9a2997
brandon <pokemonbrandon1234@gmail.com>
2021-12-02 13:23:32 +0000

    Added header names.

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ def pol_regression(features_train, y_train, degree):
 
 
 def main():
-    df = pd.read_csv('Task1 - dataset - pol_regression.csv')
+    df = pd.read_csv('Task1 - dataset - pol_regression.csv', names=['row_number', 'x', 'y'])
     x = df.x.to_numpy()
     y = df.y.to_numpy()
-    print(x, y)
 
 
 main()
