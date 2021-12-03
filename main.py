59f6c6fbb183841a797a215765fa9168c792eafe
brandon <pokemonbrandon1234@gmail.com>
2021-12-03 13:14:35 +0000

    BECOME CODE

--- main.py
+++ main.py
@@ -7,10 +7,18 @@ def pol_regression(features_train, y_train, degree):
     pass
 
 
+def getPolynomialDataMatrix(x, degree):
+    X = np.ones(x.shape)
+    for i in range(1, degree + 1):
+        X = np.column_stack((X, x ** i))
+    return X
+
+
 def main():
-    df = pd.read_csv('Task1 - dataset - pol_regression.csv', names=['row_number', 'x', 'y'])
+    df = pd.read_csv('Task1 - dataset - pol_regression.csv', names=['row_number', 'x', 'y'], skiprows=1)
     x = df.x.to_numpy()
-    y = df.y.to_numpy()
+    # y = df.y.to_numpy()
+    print(x)
 
 
 main()
