ad839dc3120482a0ee19909455747c1fd87d525c
Brandon <pokemonbrandon1234@gmail.com>
2021-12-06 17:21:42 +0000

    Reduced to a single function.

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-import csv
 import pandas as pd
 import numpy as np
 import numpy.linalg as linalg
 
 
 def pol_regression(features_train, y_train, degree):
-    pass
-
-
-def getPolynomialDataMatrix(x, degree):
-    X = np.ones(x.shape)
+    one_stack = np.ones(features_train.shape)
     for i in range(1, degree + 1):
-        X = np.column_stack((X, x ** i))
-    return X
+        one_stack = np.column_stack((one_stack, features_train ** i))
 
-
-def getWeightsForPolynomialFit(x, y, degree):
-    X = getPolynomialDataMatrix(x, degree)
-
-    XX = X.transpose().dot(X)
-    w = np.linalg.solve(XX, X.transpose().dot(y))
-    # w = np.linalg.inv(XX).dot(X.transpose().dot(y))
+    x_dot_product = one_stack.transpose().dot(one_stack)
+    w = np.linalg.solve(x_dot_product, one_stack.transpose().dot(y_train))
 
     return w
 
@@ -29,7 +18,7 @@ def main():
     df = pd.read_csv('Task1 - dataset - pol_regression.csv', names=['row_number', 'x', 'y'], skiprows=1)
     x = df.x.to_numpy()
     y = df.y.to_numpy()
-    print(getWeightsForPolynomialFit(x, y, 2))
+    print(pol_regression(x, y, 2))
 
 
 main()
