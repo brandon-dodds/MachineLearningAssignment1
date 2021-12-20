7fcc9ce728e06509fed481cb984ddb5e5b923e01
Brandon <pokemonbrandon1234@gmail.com>
2021-12-20 14:13:36 +0000

    Got my random ass centroidians.

--- Task2.py
+++ Task2.py
@@ -1,4 +1,6 @@
 import math
+import pandas as pd
+import numpy as np
 
 
 def compute_euclidian_distance(vec_1, vec_2):
@@ -6,7 +8,12 @@ def compute_euclidian_distance(vec_1, vec_2):
 
 
 def initialise_centroids(dataset, k):
-    pass
+    centroids = []
+    m = len(dataset)
+    for i in range(k):
+        r = np.random.randint(0, m - 1)
+        centroids.append(dataset[r])
+    return np.array(centroids)
 
 
 def kmeans(dataset, k):
@@ -14,7 +21,11 @@ def kmeans(dataset, k):
 
 
 def main():
-    print("Hello world!")
+    df = pd.read_csv('Task2 - dataset - dog_breeds.csv',
+                     names=['height', 'tail length', 'leg length', 'nose circumference'], skiprows=1)
+
+    random_centroids = initialise_centroids(df.values, 3)
+    print(random_centroids)
 
 
 main()
