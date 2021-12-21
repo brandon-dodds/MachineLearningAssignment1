cd9de23b81b3002a1e479b2dde0ca9ec99bac993
Brandon <pokemonbrandon1234@gmail.com>
2021-12-21 13:56:34 +0000

    Basic k-mean cluster_assignment.

--- Task2.py
+++ Task2.py
@@ -17,15 +17,21 @@ def initialise_centroids(dataset, k):
 
 
 def kmeans(dataset, k):
-    pass
+    centroids = initialise_centroids(dataset, k)
+    cluster_assignment = []
+    for points in dataset:
+        best_centroid = centroids[0]
+        for centroid in centroids[1:]:
+            if compute_euclidian_distance(points, centroid) < compute_euclidian_distance(points, best_centroid):
+                best_centroid = centroid
+        cluster_assignment.append((points, best_centroid))
+
+    return centroids, cluster_assignment
 
 
 def main():
     df = pd.read_csv('Task2 - dataset - dog_breeds.csv',
                      names=['height', 'tail length', 'leg length', 'nose circumference'], skiprows=1)
-
-    random_centroids = initialise_centroids(df.values, 3)
-    print(random_centroids)
 
 
 main()
