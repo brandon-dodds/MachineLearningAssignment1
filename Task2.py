9578b3f15cc80ae05cc95dc2aedc84e7bb55869c
Brandon <pokemonbrandon1234@gmail.com>
2021-12-26 10:47:21 +0000

    Kommented Kode

--- Task2.py
+++ Task2.py
@@ -4,6 +4,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+# Makes a scatter out of a np array from the centroids and the best centroid assigned.
 def plot_scatter_curve(k_means):
     centroids = k_means[0]
     cluster_assignment = k_means[1]
@@ -20,10 +21,12 @@ def plot_scatter_curve(k_means):
     plt.show()
 
 
+# Computes the distance utilizing the math.dist function.
 def compute_euclidian_distance(vec_1, vec_2):
     return math.dist(vec_1, vec_2)
 
 
+# Takes a random point in the dataset and assigns it to be a centroid.
 def initialise_centroids(dataset, k):
     centroids = []
     m = len(dataset)
@@ -34,12 +37,15 @@ def initialise_centroids(dataset, k):
 
 
 def kmeans(dataset, k):
+    # Initialises the cluster assignment and the centroids.
     cluster_assignment = []
     centroids = initialise_centroids(dataset, k)
     mean_changed = True
     while mean_changed:
         cluster_assignment = []
         new_centroids = []
+        # For each point in the dataset, assign the best centroid based
+        # on the euclidian distance between itself and every centroid.
         for points in dataset:
             best_centroid = centroids[0]
             for centroid in centroids[1:]:
@@ -47,6 +53,8 @@ def kmeans(dataset, k):
                     best_centroid = centroid
             cluster_assignment.append((points, best_centroid))
 
+        # For each centroid, calculate the mean of the current data points in that cluster
+        # and check if those centroids are equal to the current best centroids.
         for centroid in centroids:
             values = []
             for data in cluster_assignment:
@@ -63,11 +71,15 @@ def kmeans(dataset, k):
 
 
 def main():
+    # Read in the CSV as a pandas dataframe.
     df = pd.read_csv('Task2 - dataset - dog_breeds.csv',
                      names=['height', 'tail length', 'leg length', 'nose circumference'], skiprows=1)
 
+    # Take the height and tail length, and height and leg length values.
     height_tail_length = np.column_stack((df['height'].values, df['tail length'].values))
     height_leg_length = np.column_stack((df['height'].values, df['leg length'].values))
+
+    # plot the specific curves for k means = 2 and k means = 3
     plot_scatter_curve(kmeans(height_tail_length, 2))
     plot_scatter_curve(kmeans(height_tail_length, 3))
     plot_scatter_curve(kmeans(height_leg_length, 2))
