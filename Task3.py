00e16ca652301f3c0316d759d231b2c66afd3c10
Computing <computing@computers.com>
2022-02-02 20:51:05 +0000

    Added control and patient set boxplot.

--- Task3.py
+++ Task3.py
@@ -5,6 +5,10 @@ from matplotlib import pyplot as plt
 def main():
     dataset = pd.read_csv("Task3 - dataset - HIV RVG.csv")
     print(dataset.describe())
+    control_set = dataset.loc[dataset['Participant Condition'] == "Control"]
+    patient_set = dataset.loc[dataset['Participant Condition'] == "Patient"]
+    control_set.boxplot()
+    plt.show()
 
 
 main()
