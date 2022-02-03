5cf5c1a1e55c6faf3fde03e85811da2b5bf05841
Brandon <pokemonbrandon1234@gmail.com>
2022-02-03 15:47:16 +0000

    Taking a break got test_train_split done.

--- Task3.2.py
+++ Task3.2.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 import pandas as pd
-import sklearn.model_selection as sk
+from sklearn.model_selection import train_test_split
 
 
 def main():
     dataset = pd.read_csv("Task3 - dataset - HIV RVG.csv")
+    train, test = train_test_split(dataset, test_size=0.3)
     model = tf.keras.models.Sequential([
         tf.keras.layers.Flatten(input_shape=(28, 28)),
         tf.keras.layers.Dense(500, activation='sigmoid'),
