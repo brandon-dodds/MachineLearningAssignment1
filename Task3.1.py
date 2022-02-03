30f370350de34e50385a7dd3e209b35a7d8b16eb
Brandon <pokemonbrandon1234@gmail.com>
2022-02-03 13:45:55 +0000

    Added comments.

--- Task3.1.py
+++ Task3.1.py
@@ -2,6 +2,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 
+# Make box plot
 def make_plot(series, label):
     series.plot.box()
     plt.ylabel("Alpha")
@@ -11,11 +12,15 @@ def make_plot(series, label):
 
 def main():
     dataset = pd.read_csv("Task3 - dataset - HIV RVG.csv")
+    # Print out the mean, min, std dev
     print(dataset.describe())
+    # Take the Control and Patient data.
     control_set = dataset.loc[dataset['Participant Condition'] == "Control"]
     patient_set = dataset.loc[dataset['Participant Condition'] == "Patient"]
+    # Make the alpha plots.
     make_plot(control_set['Alpha'], "Control set")
     make_plot(patient_set['Alpha'], "Patient set")
+    # Beta plots
     plt.xlabel("Beta")
     control_set['Beta'].plot.kde()
     patient_set['Beta'].plot.kde()
