ddf12d70d7d93c34ea8fec7862583c17f0dacee3
Brandon <pokemonbrandon1234@gmail.com>
2022-02-03 13:41:23 +0000

    Functioned code.

--- Task3.1.py
+++ Task3.1.py
@@ -2,19 +2,24 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 
+def make_plot(series, label):
+    series.plot.box()
+    plt.ylabel("Alpha")
+    plt.xlabel(label)
+    plt.show()
+
+
 def main():
     dataset = pd.read_csv("Task3 - dataset - HIV RVG.csv")
     print(dataset.describe())
     control_set = dataset.loc[dataset['Participant Condition'] == "Control"]
     patient_set = dataset.loc[dataset['Participant Condition'] == "Patient"]
-    control_set['Alpha'].plot.box()
-    plt.xlabel("Control set")
-    plt.show()
-    patient_set['Alpha'].plot.box()
-    plt.xlabel("Patient set")
-    plt.show()
+    make_plot(control_set['Alpha'], "Control set")
+    make_plot(patient_set['Alpha'], "Patient set")
+    plt.xlabel("Beta")
     control_set['Beta'].plot.kde()
     patient_set['Beta'].plot.kde()
+    plt.legend(('Control set', 'Patient set'))
     plt.show()
 
 
